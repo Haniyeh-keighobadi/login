af569fba3863ecd06af40128cc1aa6cf355b93e4
Haniyeh-keighobadi <hanikey20@yahoo.com>
2023-03-26 16:10:01 +0330

    validation

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,8 +1,35 @@
 import { Component,Input,Output,EventEmitter } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+interface personalinfo{
+  FirstName:string;
+   LastName:string;
+
+}
 @Component({
     selector: 'app-login2',
     templateUrl: './login2.component.html',
     styleUrls: ['./login2.component.scss']
   })
-  export class Login2Component  {}
+  export class Login2Component  {
+    
+    ngOnInit(): void {
+    }
+    dataSource:any;
+    dataSourceSelect:any;
+
+    FirstName:string='';
+    LastName:string='';
+   
+    personList:personalinfo[]=[];
+   
+    getDataGrid1() {
+
+      let newperson:personalinfo={
+        FirstName:this.FirstName,
+        LastName:this.LastName,
+      
+      }}
+ 
+ 
+  
+  }
